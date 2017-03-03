2d48d7500c0d5f858278757ae47e25a50bbdd5fc
Jeromy <jeromyj@gmail.com>
2017-03-03 00:06:03 -0800

    add transactions api bindings

---
+++ transactions.go
@@ -0,0 +1,97 @@
+package zcash
+
+import (
+	"fmt"
+
+	jrpc "github.com/whyrusleeping/jrpc"
+)
+
+type Client struct {
+	cli *jrpc.Client
+}
+
+func NewZcashClient(host, user, pass string) *Client {
+	return &Client{
+		cli: &jrpc.Client{
+			Host: host,
+			User: user,
+			Pass: pass,
+		},
+	}
+}
+
+func (c *Client) SignRawTransaction(rawtx string) (string, error) {
+	req := &jrpc.Request{
+		Method: "signrawtransaction",
+		Params: []string{rawtx},
+	}
+
+	var sigout struct {
+		Result struct {
+			Hex string
+		}
+	}
+
+	err := c.cli.Do(req, &sigout)
+	if err != nil {
+		return "", err
+	}
+	return sigout.Result.Hex, nil
+}
+
+func (c *Client) SendRawTransaction(sigtx string) (string, error) {
+	req := &jrpc.Request{
+		Method: "sendrawtransaction",
+		Params: []string{sigtx},
+	}
+
+	resp := jrpc.Response{ResultType: ""}
+	err := c.cli.Do(req, &resp)
+	if err != nil {
+		return "", err
+	}
+	if resp.Error != nil {
+		return "", resp.Error
+	}
+
+	return *resp.Result.(*string), nil
+}
+
+type infoTxOut struct {
+	Address  string
+	Amount   float64
+	Category string
+	Vout     int
+	Size     int
+}
+
+type infoTransaction struct {
+	Amount        float64
+	Confirmations int
+	Blockhash     string
+	Blockindex    int
+	Blocktime     int64
+	Details       []infoTxOut
+}
+
+func (c *Client) GetTransactionValue(tx, addr string) (float64, error) {
+	req := &jrpc.Request{
+		Method: "gettransaction",
+		Params: []string{tx},
+	}
+
+	var out struct {
+		Result infoTransaction
+	}
+	err := c.cli.Do(req, &out)
+	if err != nil {
+		return 0, err
+	}
+
+	for _, res := range out.Result.Details {
+		if res.Address == addr && res.Category == "receive" {
+			return res.Amount, nil
+		}
+	}
+	return 0, fmt.Errorf("no outputs for given address found")
+}
